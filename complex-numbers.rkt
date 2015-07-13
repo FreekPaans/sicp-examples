2d5028f275c18f97789b2c597e58e4933510246d
Freek Paans <freekpaans@gmail.com>
2015-07-13 20:09:06 +0200

    starting complex number data abstraction example

---
+++ complex-numbers.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(define (real-part z)
+  '?)
+
+(define (imag-part z)
+  '?)
+
+(define (magnitude z)
+  '?)
+
+(define (angle z)
+  '?)
+
+(define (make-from-real-imag x y)
+  '?)
+
+(define (make-from-mag-ang r a)
+  '?)
+
