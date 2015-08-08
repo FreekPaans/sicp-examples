b629d04e02977ecb78d8d45f48677d8e26412b53
Freek Paans <freekpaans@gmail.com>
2015-08-08 19:58:12 +0200

    separated representation

--- meta-circular.rkt
+++ meta-circular.rkt
@@ -4,9 +4,33 @@
 
 (define (self-evaluating-exp? exp)
   (or (number? exp) (symbol? exp) (boolean? exp) (string? exp)))
-
 (define (operator exp) (car exp))
 (define (operands exp) (cdr exp))
+(define (combination? exp)
+  (pair? exp))
+(define (if-exp? exp)
+  (and (pair? exp) (eq? (car exp) 'if)))
+(define (quoted-exp? exp)
+  (and (pair? exp) (eq? (car exp) 'quote)))
+(define (begin-exp? exp)
+  (and (pair? exp) (eq? (car exp) 'begin)))
+(define (begin-actions exp)
+  (cdr exp))
+(define (application? exp)
+  #t)
+(define (first-operand exps)
+  (car exps))
+(define (no-operands? exps)
+  (null? exps))
+(define (remaining-operands exps)
+  (cdr exps))
+(define (if-predicate exp) (cadr exp))
+(define (if-consequent exp) (caddr exp))
+(define (if-alternative exp) (cadddr exp))
+(define (true? val) (if val #t #f))
+(define (last-exp? exps) (null? (cdr exps)))
+(define (first-exp exps) (car exps))
+(define (remaining-exps exps) (cdr exps))
 
 (define (get-primitive-proc symbol)
   (cond
@@ -18,54 +42,21 @@
     ((eq? 'display symbol) display)
     (else (error "unknown symbol " symbol))))
 
-(define (combination? exp)
-  (pair? exp))
-
 (define (apply-exp operator operands)
   (apply (get-primitive-proc operator) operands))
 
-
 (define (eval-if exp)
-  (define (if-predicate exp) (cadr exp))
-  (define (if-consequent exp) (caddr exp))
-  (define (if-alternative exp) (cadddr exp))
-  (define (true? val) (if val #t #f))
   (if (true? (eval-exp (if-predicate exp)))
       (eval-exp (if-consequent exp))
       (eval-exp (if-alternative exp))))
 
-(define (if-exp? exp)
-  (and (pair? exp) (eq? (car exp) 'if)))
-
-(define (quoted-exp? exp)
-  (and (pair? exp) (eq? (car exp) 'quote)))
-
-(define (begin-exp? exp)
-  (and (pair? exp) (eq? (car exp) 'begin)))
-
 (define (eval-sequence exp)
-  (define (last-exp? exps) (null? (cdr exps)))
-  (define (first-exp exps) (car exps))
-  (define (remaining-exps exps) (cdr exps))
   (define (iter-seq exps)
     (cond ((last-exp? exps) (eval-exp (first-exp exps)))
           (else
            (eval-exp (first-exp exps))
            (iter-seq (remaining-exps exps)))))
   (iter-seq exp))
-
-(define (begin-actions exp)
-  (cdr exp))
-
-(define (application? exp)
-  #t)
-
-(define (first-operand exps)
-  (car exps))
-(define (no-operands? exps)
-  (null? exps))
-(define (remaining-operands exps)
-  (cdr exps))
 
 (define (list-of-values exps)
   (cond ((no-operands? exps) '())
@@ -83,6 +74,6 @@
     (else (error "unknown expression " exp))))
   
 
-(eval-exp '(+ (begin (display "1") 1) (begin (display "2") 2)))
+(eval-exp '(+  1 2))
 ;(eval-exp '(+ 1 2))
 
